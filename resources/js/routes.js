aefa8aabaad812083c8e6df602a336f9f65fc318
Jovert Lota Palonpon <palonponjovertlota@gmail.com>
2019-03-22 17:14:18 +0800

    wip

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,16 +1,24 @@
+import Signin from './views/auth/Signin';
 import Home from './views/admin/Home';
 import CategoryList from './views/admin/categories/List';
 import ErrorNotFound from './views/errors/NotFound';
 
 export const routes = [
     {
+        path: '/signin',
+        component: Signin,
+    },
+
+    {
         path: '/',
         component: Home,
+        auth: true,
     },
 
     {
         path: '/categories',
         component: CategoryList,
+        auth: true,
     },
 
     {
